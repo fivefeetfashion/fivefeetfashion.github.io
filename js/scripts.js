0934b6c79c272d618b2893aa6c0096af0359c4e3
vritant24 <vritoku@gmail.com>
2017-06-05 12:44:17 +0530

    added script to control close button

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    attachCloseToTop();
+    if($(document).width() < 1025) {
+      attachCloseToTop();
+    }
+  });
+  $( window ).resize(function() {
+    if($(document).width() < 1025) {
+      $('#blog-post').addClass('relative-position')
+      attachCloseToTop();
+    } else {
+      $('.close-button').removeClass('close-button-absolute');
+      $('.close-button').addClass('close-button-fixed');
+      $('#blog-post').removeClass('relative-position')
+    }
   });
 });
 
 
 attachCloseToTop = function() {
-  if($(document).width() < 1025) {
-    if(!($('#blog-post').hasClass('hide'))) {
-      var distanceFromTop = $(this).scrollTop();
-      if (distanceFromTop >= $('.sidebar').height()) {
-          $('.close-button').removeClass('close-button-absolute');
-          $('.close-button').addClass('close-button-fixed');
-      } else {
-          $('.close-button').removeClass('close-button-fixed');
-          $('.close-button').addClass('close-button-absolute');
-      }
+  if(!($('#blog-post').hasClass('hide'))) {
+    var distanceFromTop = $(this).scrollTop();
+    if (distanceFromTop >= $('.sidebar').height()) {
+        $('.close-button').removeClass('close-button-absolute');
+        $('.close-button').addClass('close-button-fixed');
+    } else {
+        $('.close-button').removeClass('close-button-fixed');
+        $('.close-button').addClass('close-button-absolute');
     }
   }
 };
